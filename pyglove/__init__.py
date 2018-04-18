3451501f05e9d23c6980df5ed21b647f78533e62
Robert Pugh <robertpugh@Roberts-MBP.etsclient.org>
2018-04-18 14:30:28 -0700

    make pyglove installable

---
+++ pyglove/__init__.py
@@ -0,0 +1,1 @@
+from .pyglove import GloVe
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyglove',
+      version='0.1',
+      description='Python wrapper for using GloVe vectors',
+      url='https://github.com/Lguyogiro/pyglove',
+      author='Lguyogiro',
+      license='MIT',
+      packages=['pyglove'],
+      zip_safe=False)
